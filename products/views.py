54d08b8241da46c184289c82a338f18f348db5f4
arnaud-eb <arnaud.depierreux@easebest.com>
2019-09-20 11:32:04 +0200

    avoid duplicate product

--- products/views.py
+++ products/views.py
@@ -10,19 +10,23 @@ def home(request):
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
-            product = Product()
-            product.title = request.POST['title']
-            product.body = request.POST['body']
-            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
-                product.url = request.POST['url']
-            else:
-                product.url = "http://" + request.POST['url']
-            product.icon = request.FILES['icon']
-            product.image = request.FILES['image']
-            product.pub_date = timezone.datetime.now()
-            product.hunter = request.user
-            product.save()
-            return redirect('home')
+            try:
+                product = Product.objects.get(title=request.POST['title'])
+                return render(request,'products/create.html', {'error':'Product name has already been taken'})
+            except Product.DoesNotExist:
+                product = Product()
+                product.title = request.POST['title']
+                product.body = request.POST['body']
+                if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                    product.url = request.POST['url']
+                else:
+                    product.url = "http://" + request.POST['url']
+                product.icon = request.FILES['icon']
+                product.image = request.FILES['image']
+                product.pub_date = timezone.datetime.now()
+                product.hunter = request.user
+                product.save()
+                return redirect('home')
         else:
             return render(request,'products/create.html', {'error':'All fields are required'})
     else:

d7248a2e51be7c72ed3139fa677ad455b77b0d56
arnaud-eb <arnaud.depierreux@easebest.com>
2019-09-22 15:49:56 +0200

    Icon, product details and home page

--- products/models.py
+++ products/models.py
@@ -14,5 +14,8 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def summary(self):
+        return self.body[:200]+'...'
+
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
--- products/views.py
+++ products/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
 
 def home(request):
-    return render(request,'products/home.html')
+    products = Product.objects.order_by('pub_date')
+    return render(request,'products/home.html',{'products':products})
 
-@login_required
+@login_required(login_url='/account/signup')
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
@@ -17,7 +18,7 @@ def create(request):
                 product = Product()
                 product.title = request.POST['title']
                 product.body = request.POST['body']
-                if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                if request.POST['url'].lower().startswith('http://') or request.POST['url'].lower().startswith('https://'):
                     product.url = request.POST['url']
                 else:
                     product.url = "http://" + request.POST['url']
@@ -26,8 +27,20 @@ def create(request):
                 product.pub_date = timezone.datetime.now()
                 product.hunter = request.user
                 product.save()
-                return redirect('home')
+                return redirect('/products/'+ str(product.id))
         else:
             return render(request,'products/create.html', {'error':'All fields are required'})
     else:
         return render(request,'products/create.html')
+
+def detail(request, product_id):
+    detailproduct=get_object_or_404(Product, pk=product_id)
+    return render (request,'products/detail.html',{'product':detailproduct})
+
+@login_required(login_url='/account/signup')
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product=get_object_or_404(Product, pk=product_id)
+        product.votes_total+=1
+        product.save()
+        return redirect('/products/'+ str(product.id))
